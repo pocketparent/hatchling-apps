db07aff7c6cd75e3edca1557e12cd39dbb85f7b7
pocketparent <dianamezzanotte@gmail.com>
2025-04-22 12:55:26 -0400

    Create firebase.ts

---
+++ src/config/firebase.ts
@@ -0,0 +1,7 @@
+// src/config/firebase.ts
+
+import '@react-native-firebase/app';
+import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
+
+export { auth, firestore };
